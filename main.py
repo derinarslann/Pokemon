b93be0cf3c339f006f0cac944fb64c5aabe1eec9
derinarslan <derinarslan2020@gmail.com>
2025-02-12 20:40:13 +0300

    derin

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 from config import token
-from logic import Pokemon
+from logic import Pokemon, Wizard, Fighter
+import random
 
 # Bot için niyetleri (intents) ayarlama
 intents = discord.Intents.all()  # Varsayılan ayarların alınması
@@ -23,16 +24,42 @@ async def go(ctx):
     author = ctx.author.name  # Mesaj yazarının adını alma
     # Kullanıcının zaten bir Pokémon'u olup olmadığını kontrol edin. Eğer yoksa, o zaman...
     if author not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(author)  # Yeni bir Pokémon oluşturma
+        x = random.randint(1,3)
+        if x == 1:
+            pokemon = Pokemon(author)  # Yeni bir Pokémon oluşturma
+        elif x == 2:
+            pokemon = Fighter(author)  # Yeni bir Pokémon oluşturma
+        elif x == 1:
+            pokemon = Wizard(author)  # Yeni bir Pokémon oluşturma
+        else:
+            print("HATAAA!")
         await ctx.send(await pokemon.info())  # Pokémon hakkında bilgi gönderilmesi
         image_url = await pokemon.show_img()  # Pokémon resminin URL'sini alma
         if image_url:
             embed = discord.Embed()  # Gömülü mesajı oluşturma
             embed.set_image(url=image_url)  # Pokémon'un görüntüsünün ayarlanması
             await ctx.send(embed=embed)  # Görüntü içeren gömülü bir mesaj gönderme
+            await ctx.send(Pokemon.pokemons)
         else:
             await ctx.send("Pokémonun görüntüsü yüklenemedi!")
     else:
+        await ctx.send(Pokemon.pokemons)
         await ctx.send("Zaten kendi Pokémonunuzu oluşturdunuz!")  # Bir Pokémon'un daha önce yaratılıp yaratılmadığını gösteren bir mesaj
+
+@bot.command()
+async def attack(ctx ,enemy):
+    if enemy in Pokemon.pokemons:
+        saldıran= Pokemon.pokemons[ctx.author.name]
+        savunan= Pokemon.pokemons[enemy]
+        sonuç= await saldıran.saldır(savunan)
+        await ctx.send(sonuç)
+    else:
+        await ctx.send("düşman bulunamadı")
+
+@bot.command()
+async def feed(ctx):
+    pass
+
+
 # Botun çalıştırılması
 bot.run(token)
